be646873aa3448f0251616f1fcf28568397fd6ff
rob <robbiestells@hotmail.com>
2017-03-06 14:38:55 -0500

    feat: Add support for all post types
    Added layout files for Event and Link post
    types and created framework for Adapters

--- app/src/main/java/adapters/FeedAdapter.java
+++ app/src/main/java/adapters/FeedAdapter.java
@@ -1,24 +1,17 @@
 package adapters;
 
 import android.content.Context;
-import android.net.Uri;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
-import com.primeperspective.jazzreader.MainActivity;
 import com.primeperspective.jazzreader.R;
 
 import java.util.ArrayList;
@@ -30,9 +23,11 @@ import models.FeedItem;
  */
 
 public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
-    private static final int ITEM_TYPE_NORMAL = 0;
+    private static final int ITEM_TYPE_NEWS = 0;
     private static final int ITEM_TYPE_RELEASE = 1;
     private static final int ITEM_TYPE_VIDEO = 2;
+    private static final int ITEM_TYPE_LINK = 3;
+    private static final int ITEM_TYPE_EVENT = 4;
 
     ArrayList<FeedItem> feedItems;
     Context context;
@@ -49,8 +44,13 @@ public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
             return ITEM_TYPE_RELEASE;
         } else if (current.getType().contains("video")) {
             return ITEM_TYPE_VIDEO;
+        } else if (current.getType().contains("link")) {
+            return ITEM_TYPE_LINK;
+        } else if (current.getType().contains("event")) {
+            return ITEM_TYPE_EVENT;
         } else {
-            return ITEM_TYPE_NORMAL;
+            //news event
+            return ITEM_TYPE_NEWS;
         }
 
     }
@@ -59,21 +59,29 @@ public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         ViewHolder holder;
         switch (viewType) {
-            case ITEM_TYPE_NORMAL:
-                View normalView = LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_item, parent, false);
-                holder = new NormalViewHolder(normalView);
+            case ITEM_TYPE_NEWS:
+                View normalView = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_post_type, parent, false);
+                holder = new NewsViewHolder(normalView);
                 break;
             case ITEM_TYPE_RELEASE:
-                View releaseView = LayoutInflater.from(parent.getContext()).inflate(R.layout.second_post_type, parent, false);
+                View releaseView = LayoutInflater.from(parent.getContext()).inflate(R.layout.release_post_type, parent, false);
                 holder = new ReleaseViewHolder(releaseView);
                 break;
             case ITEM_TYPE_VIDEO:
                 View videoView = LayoutInflater.from(parent.getContext()).inflate(R.layout.video_post_type, parent, false);
                 holder = new VideoViewHolder(videoView);
                 break;
+            case ITEM_TYPE_EVENT:
+                View eventView = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_post_type, parent, false);
+                holder = new EventViewHolder(eventView);
+                break;
+            case ITEM_TYPE_LINK:
+                View linkView = LayoutInflater.from(parent.getContext()).inflate(R.layout.link_post_type, parent, false);
+                holder = new LinkViewHolder(linkView);
+                break;
             default:
-                View defaultView = LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_item, parent, false);
-                holder = new NormalViewHolder(defaultView);
+                View defaultView = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_post_type, parent, false);
+                holder = new NewsViewHolder(defaultView);
                 break;
         }
 
@@ -86,8 +94,8 @@ public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
         final FeedItem currentItem = feedItems.get(position);
 
         switch (itemType) {
-            case ITEM_TYPE_NORMAL:
-                ((NormalViewHolder) holder).bindData(currentItem);
+            case ITEM_TYPE_NEWS:
+                ((NewsViewHolder) holder).bindData(currentItem);
                 break;
             case ITEM_TYPE_RELEASE:
                 ((ReleaseViewHolder) holder).bindData(currentItem);
@@ -95,8 +103,14 @@ public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
             case ITEM_TYPE_VIDEO:
                 ((VideoViewHolder) holder).bindData(currentItem);
                 break;
+            case ITEM_TYPE_EVENT:
+                ((EventViewHolder) holder).bindData(currentItem);
+                break;
+            case ITEM_TYPE_LINK:
+                ((LinkViewHolder) holder).bindData(currentItem);
+                break;
             default:
-                ((NormalViewHolder) holder).bindData(currentItem);
+                ((NewsViewHolder) holder).bindData(currentItem);
                 break;
             //set feed item data
 //        final FeedItem current = feedItems.get(position);
@@ -120,12 +134,12 @@ public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
         return feedItems.size();
     }
 
-    public class NormalViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView Title, Date, Type;
         ImageView Image;
         CardView cardView;
 
-        public NormalViewHolder(View itemView) {
+        public NewsViewHolder(View itemView) {
             super(itemView);
 
             itemView.setOnClickListener(this);
@@ -222,5 +236,77 @@ public class FeedAdapter extends RecyclerView.Adapter<ViewHolder> {
 
         }
 
+    }
+
+    public class EventViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView Title, Date, Type;
+        ImageView Image;
+        CardView cardView;
+
+        public EventViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+
+            //get views
+//            Title = (TextView) itemView.findViewById(R.id.postTitle);
+//            Type = (TextView) itemView.findViewById(R.id.postType);
+//            Date = (TextView) itemView.findViewById(R.id.postDate);
+//            Image = (ImageView) itemView.findViewById(R.id.postImage);
+//            cardView = (CardView) itemView.findViewById(R.id.feedItemCard);
+        }
+
+        public void bindData(FeedItem item) {
+
+//            Title.setText(item.getTitle());
+//            Date.setText(item.getCreatedAt());
+//            Type.setText(item.getType());
+//            Glide.with(context).load(item.getImageLink()).into(Image);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //get episode that was clicked on and pass to main activity
+//            FeedItem selected = feedItems.get(getPosition());
+//
+//            MainActivity mainActivity = MainActivity.getInstance();
+//            mainActivity.OnEpisodeSelected(selected);
+        }
+    }
+
+    public class LinkViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView Title, Date, Type;
+        ImageView Image;
+        CardView cardView;
+
+        public LinkViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+
+            //get views
+//            Title = (TextView) itemView.findViewById(R.id.postTitle);
+//            Type = (TextView) itemView.findViewById(R.id.postType);
+//            Date = (TextView) itemView.findViewById(R.id.postDate);
+//            Image = (ImageView) itemView.findViewById(R.id.postImage);
+//            cardView = (CardView) itemView.findViewById(R.id.feedItemCard);
+        }
+
+        public void bindData(FeedItem item) {
+
+//            Title.setText(item.getTitle());
+//            Date.setText(item.getCreatedAt());
+//            Type.setText(item.getType());
+//            Glide.with(context).load(item.getImageLink()).into(Image);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //get episode that was clicked on and pass to main activity
+//            FeedItem selected = feedItems.get(getPosition());
+//
+//            MainActivity mainActivity = MainActivity.getInstance();
+//            mainActivity.OnEpisodeSelected(selected);
+        }
     }
 }
